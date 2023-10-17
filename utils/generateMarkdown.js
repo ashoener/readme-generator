d14b23caeb8c022e69c8b048ee08d6d54aae3b4a
Andrew Shoener <a.b.shoener@gmail.com>
2023-10-17 10:44:11 -0500

    Render the sections

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,25 +15,62 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license)
+    return `## License
+
+This project is covered under the ${license} license. You may view it [here](/LICENSE).
+`;
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   const sections = [
     "Description",
+    "Installation",
     "Usage",
     "How to Contribute",
     "Tests",
+    "Questions",
     "License",
   ];
   return `# ${data.title}
 ${renderLicenseBadge(data.license)}  
+
+## Description
+
+${data.description}
 
 ## Table of Contents
 
 ${sections
   .map((s) => `- [${s}](#${s.toLowerCase().split(" ").join("-")})`)
   .join("\n")}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## How to Contribute
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+You can contact me on [GitHub](${data.github}) or by [email](mailto:${
+    data.email
+  }).
+
+${renderLicenseSection(data.license)}
 `;
 }
 
